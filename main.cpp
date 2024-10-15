380da09d7b295cf065dd05e90c7241c725520223
Amrita Bhowmick <milanbhwmick12341234@gmail.com>
2024-10-16 00:30:52 +0530

    modify code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,64 @@
-#include<iostream>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
-int main(){
+void gamecode(int n, int attempt)
+{
+    int guess;
+    int count = 0;
+    while (count < attempt)
+    {
+        cout << "\nEnter your number: ";
+        cin >> guess;
+        if (guess > n)
+        {
+            cout << "\nToo large number. Try again.";
+            count++;
+        }
+        else if (guess < n)
+        {
+            cout << "\nToo small number. Try again.";
+            count++;
+        }
+        else
+        {
+            cout << "\nYour number is right now..";
+            break;
+        }
+    }
+}
+void easy()
+{
+    srand(time(0));
+    int n = rand();
+    n = (n % 10) + 1;
+    gamecode(n, 5);
 
+}
+void medium()
+{
+    srand(time(0));
+    int n = rand();
+    n = (n % 100) + 1;
+    gamecode(n, 7);
+}
+void hard();
+int main()
+{
+    int level;
+    cout << "\nEnter the level for this game: ";
+    cin >> level;
+    if (level == 1)
+    {
+        easy();
+    }
+    else if (level == 2)
+    {
+        medium();
+    }
+    else if (level == 3)
+    {
+        // hard();
+    }
     return 0;
 }
